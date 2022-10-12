73e7b47ef8352a8dbbac024e3dd75c171d4d7736
Nidhi Sawhney <nidhi.sawhney@sap.com>
2022-10-12 15:41:48 +0200

    Using port 9001 instead of 5000

--- AICore/app.py
+++ AICore/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import logging
+import os
 
 app = Flask(__name__)
 
@@ -49,4 +50,6 @@ def callPyomo():
     return optimizer.make_change_pyomo(int(amount),coins)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    print(f'{os.environ["greetingmessage"]}')
+    print("Serving Started")
+    app.run(debug=True, host='0.0.0.0',port=9001)
